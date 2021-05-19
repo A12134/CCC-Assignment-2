805b690363d9d2b274df396760d973a9dd7f6009
Yuling Zhou <44768687+lingling6strings@users.noreply.github.com>
2021-05-19 16:30:34 +0800

    Update analysis.js

--- couchDB/analysis.js
+++ couchDB/analysis.js
@@ -1,12 +1,12 @@
 {
   "views": {
-      "city": {
-          "reduce": "_sum",
-          "map": "function (doc) {\r\n  if(doc.User_Location_Type === \"city\" && doc.User_Location !== \"\"){\r\n    emit([doc.Period, doc.User_Location, \"Polarity\"], doc.Polarity);\r\n    if(doc.Polarity > 0){\r\n      emit([doc.Period, doc.User_Location, \"positiveCount\"],1);\r\n    }else if(doc.Polarity === 0){\r\n    emit([doc.Period, doc.User_Location, \"nuetralCount\"],1);\r\n    }else if(doc.Polarity < 0){\r\n      emit([doc.Period, doc.User_Location, \"negativeCount\"],1);\r\n    }\r\n  } else if(doc.User_location_Type === \"city\" && doc.User_location !== \"\"){\r\n    emit([doc.Period, doc.User_location, \"Polarity\"], doc.Polarity);\r\n    if(doc.Polarity > 0){\r\n      emit([doc.Period, doc.User_location, \"positiveCount\"],1);\r\n    }else if(doc.Polarity === 0){\r\n      emit([doc.Period, doc.User_location, \"nuetralCount\"],1);\r\n    }else if(doc.Polarity < 0){\r\n      emit([doc.Period, doc.User_location, \"negativeCount\"],1);\r\n    }\r\n  }\r\n}"
-      }
+    "suburb": {
+      "reduce": "_sum",
+      "map": "function (doc) {\r\n    emit([doc.Period, doc.User_Location.address.city, doc.User_Location.address.suburb, \"Polarity\"], doc.Polarity);\r\n    if(doc.Polarity > 0){\r\n      emit([doc.Period, doc.User_Location.address.city, doc.User_Location.address.suburb, \"positiveCount\"],1);\r\n    }else if(doc.Polarity === 0){\r\n      emit([doc.Period, doc.User_Location.address.city, doc.User_Location.address.suburb, \"nuetralCount\"],1);\r\n    }else if(doc.Polarity < 0){\r\n      emit([doc.Period, doc.User_Location.address.city, doc.User_Location.address.suburb, \"negativeCount\"],1);\r\n    }\r\n}"
+    }
   },
   "language": "javascript",
   "options": {
-      "partitioned": false
+    "partitioned": false
   }
 }
