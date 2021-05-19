2b254ebdf843ef36fc485c632cd6a2588189a8af
Yuling Zhou <44768687+lingling6strings@users.noreply.github.com>
2021-05-19 19:18:25 +0800

    Update getViewData.R

--- couchDB/getViewData.R
+++ couchDB/getViewData.R
@@ -1,10 +1,10 @@
+library(sf)
 library(httr)
-library(RCurl)
 
 myviewURL = "http://172.26.132.19:5984/twitter/_design/analysis/_view/suburb?reduce=true&group_level=4"
 viewData = GET(url = myviewURL, 
                authenticate("admin","admin")
-               ) %>% content()
+) %>% content()
 viewData = viewData$rows
 
 periods = c("2020 1-3", "2020 4-6", "2020 10-12", "2021 1-3")
@@ -29,22 +29,24 @@ for (i in 1:length(viewData)){
   suburb = keys[3][[1]]
   feature = keys[4][[1]]
   value = row$value
-  if(!suburb %in% data$suburb){
-    for(j in 1:4){
-      n = nrow(data)
-      data[n+1,1]= periods[j]
-      data[n+1,2]= city
-      data[n+1,3]= suburb
+  if(!is.null(city) & !is.null(suburb)){
+    if(!suburb %in% data$suburb){
+      for(j in 1:4){
+        n = nrow(data)
+        data[n+1,1]= periods[j]
+        data[n+1,2]= city
+        data[n+1,3]= suburb
+      }
     }
-  }
-  if(feature == "Polarity"){
-    data[which(data$time==time & data$city==city & data$suburb==suburb),7] = value
-  }else if(feature == "positiveCount"){
-    data[which(data$time==time & data$city==city & data$suburb==suburb),4] = value
-  }else if(feature == "negativeCount"){
-    data[which(data$time==time & data$city==city & data$suburb==suburb),5] = value
-  }else if(feature == "nuetralCount"){
-    data[which(data$time==time & data$city==city & data$suburb==suburb),6] = value
+    if(feature == "Polarity"){
+      data[which(data$time==time & data$city==city & data$suburb==suburb),7] = value
+    }else if(feature == "positiveCount"){
+      data[which(data$time==time & data$city==city & data$suburb==suburb),4] = value
+    }else if(feature == "negativeCount"){
+      data[which(data$time==time & data$city==city & data$suburb==suburb),5] = value
+    }else if(feature == "nuetralCount"){
+      data[which(data$time==time & data$city==city & data$suburb==suburb),6] = value
+    }
   }
 }
 data = data[-1,]
@@ -54,3 +56,4 @@ data$posPercent = data$posCount/data$totalCount
 data$negPercent = data$negCount/data$totalCount
 data$neuPercent = data$neuCount/data$totalCount
 data$polarityAvg = data$polaritySum/data$totalCount
+sum(data$totalCount)
